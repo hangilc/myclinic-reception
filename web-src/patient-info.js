7a27be75e8d1513df974ba62bef0111d3e9a5c12
hangilc <hangil@chang.jp>
2016-12-15 23:14:25 +0900

    reorganized patient-info

--- web-src/patient-info.js
+++ web-src/patient-info.js
@@ -10,11 +10,68 @@ var RoujinDisp = require("./roujin-disp.js");
 var KouhiDisp = require("./kouhi-disp.js");
 var CommandBox = require("./patient-info-command-box.js");
 var ShahokokuhoForm = require("./shahokokuho-form.js");
+var tmplSrc = require("raw!./patient-info.html");
 
 exports.add = function(data){
 	var patient = data.patient;
 	var hoken = data.hoken;
+	var sub;
+	console.log(hoken);
 	Panel.add("患者情報", function(dom, wrapper){
+		dom.innerHTML = tmplSrc;
+		sub = Subpanel.create("基本情報", function(subdom){
+			BasicInfo.render(subdom, patient);
+		});
+		dom.querySelector(".basic-info-wrapper").appendChild(sub);
+		if( hoken.shahokokuho_list.length > 0 ){
+			sub = Subpanel.create("社保・国保", function(subdom){
+				ShahokokuhoDisp.render(subdom, hoken.shahokokuho_list[0]);
+			});
+			dom.querySelector(".shahokokuho-wrapper").appendChild(sub);
+		}
+		if( hoken.koukikourei_list.length > 0 ){
+			sub = Subpanel.create("後期高齢", function(subdom){
+				KoukikoureiDisp.render(subdom, hoken.koukikourei_list[0]);
+			});
+			dom.querySelector(".koukikourei-wrapper").appendChild(sub);
+		}
+		if( hoken.roujin_list.length > 0 ){
+			sub = Subpanel.create("老人保険", function(subdom){
+				RoujinDisp.render(subdom, hoken.roujin_list[0]);
+			});
+			dom.querySelector(".roujin-wrapper").appendChild(sub);
+		}
+		hoken.kouhi_list.forEach(function(kouhi, index){
+			sub = Subpanel.create("公費(" + (index+1) + ")", function(subdom){
+				KouhiDisp.render(subdom, kouhi);
+			});
+			dom.querySelector(".kouhi-wrapper").appendChild(sub);
+		});
+		var commandBox = CommandBox.create(patient.patient_id, {
+			onNewShahokokuho: function(){
+				newShahokokuho(patient, wrapper);
+			},
+			onNewKoukikourei: function(){
+				console.log("new-koukikourei");
+			},
+			onNewKouhi: function(){
+				console.log("new-kouhi");
+
+			},
+			onEditAllHoken: function(){
+				console.log("edit-all-hoken");
+			},
+			onStartVisit: function(){
+				console.log("start-visit");
+
+			},
+			onClose: function(){
+				wrapper.parentNode.removeChild(wrapper);	
+			}
+		});
+		dom.appendChild(commandBox);
+		return;
+
 		wrapper.classList.add("listening-to-shahokokuho-entered");
 		wrapper.addEventListener("shahokokuho-entered", function(event){
 			var shahokokuho = event.detail;
